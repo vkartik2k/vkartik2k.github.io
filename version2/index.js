c32169a871ffe6f08240dc2ed28170284ba71dec
vkartik2k <vkartik2k@gmail.com>
2019-06-06 13:13:21 +0530

    version 2 final

--- version2/index.js
+++
@@ -1,7 +0,0 @@
-setTimeout(function () {
-    let letterArr = $(".mainNameInner")
-    letterArr.each(function() {
-        $( this ).addClass( "animated bounce" );
-        console.log("Animation")
-      }); 
-},2000)

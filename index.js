93771866804731eaf589a6db8fa033016f35b042
vkartik2k <vkartik2k@gmail.com>
2021-03-06 21:44:45 +0530

    a

--- index.js
+++ index.js
@@ -1,108 +1,58 @@
-$(function () {
-  // **********************************************************************************************************
-  //                                            ANIMATION FOR LOADING
-  setTimeout(function(){
-    $("#backgroundBlur1").hide()
-    $("#downBtn").hide()
-    $("#loading").addClass("animated slideOutUp")
-  },2500)
-  setTimeout(function(){
-    $("#backgroundBlur1").show()
-    $('#backgroundBlur1').addClass("animated fadeIn")
-  },2800)
-  setTimeout(function(){
-    $("#downBtn").show()
-    $('#downBtn').addClass("animated jackInTheBox")
-  },3600)
-  setTimeout(function(){
-    $("#downBtn").removeClass('animated jackInTheBox');
-  },4000)
+$(document).ready(function () {
+	new GitHubCalendar(".calendar", "vkartik2k");
 
-  // **********************************************************************************************************
-  //                                            ANIMATION FOR REVEAL ON SCROLL
-  var $window = $(window), win_height_padded = $window.height() * 1.1, isTouch = Modernizr.touch;
-    
-  function revealOnScroll() {
-    var scrolled = $window.scrollTop();
-    $(".revealOnScroll:not(.animated)").each(function () {
-      var $this     = $(this),
-          offsetTop = $this.offset().top;
+	fetch('https://codeforces.com/api/user.rating?handle=vkartik2k').then(function (response) {
+		return response.json();
+	}).then(function (data) {
+		let rating = data.result[data.result.length - 1].newRating
+		document.getElementById('cfRating').innerHTML = rating
+	}).catch(function () {
+		console.log("Booo");
+	})
 
-      if (scrolled + win_height_padded > offsetTop) {
-        if ($this.data('timeout')) {
-          window.setTimeout(function(){
-            $this.addClass('animated ' + $this.data('animation'));
-          }, parseInt($this.data('timeout'),10));
-        } else {
-          $this.addClass('animated ' + $this.data('animation'));
-        }
-      }
-  })}
+	fetch('https://api.github.com/users/vkartik2k/repos').then(function (response) {
+		return response.json();
+	}).then(function (data) {
+		let pr = data.length
+		document.getElementById('githubRepos').innerHTML = pr
+	}).catch(function () {
+		console.log("Booo");
+	})
+	$(document).on("scroll", onScroll);
 
-  $window.on('scroll', revealOnScroll);
+	//smoothscroll
+	$('a[href^="#"]').on('click', function (e) {
+		e.preventDefault();
+		$(document).off("scroll");
 
-  // **********************************************************************************************************
-  //                                            DOWN BTN ANIMATION
+		$('a').each(function () {
+			$(this).removeClass('selected');
+		})
+		$(this).addClass('selected');
 
-  $("#downBtn").hover(function(){
-    $(this).addClass('animated rubberBand');
-  });
-  $("#downBtn").bind("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd",function(){
-    $(this).removeClass('animated rubberBand');
-  });
+		var target = this.hash,
+			menu = target;
+		$target = $(target);
+		$('html, body').stop().animate({
+			'scrollTop': $target.offset().top + 2
+		}, 500, 'swing', function () {
+			window.location.hash = target;
+			$(document).on("scroll", onScroll);
+		});
+	});
+});
 
-  // **********************************************************************************************************
-  //                                            DYNAMIC PROJECT CONTENT
-
-  $(".sliderBtn").on("click",function () {
-    $('.sliderBtn.selected').removeClass('selected');
-    $(this).addClass('selected');
-    var id = $(this).attr('id');
-    var projectHeading = ["TIC-TAC-TOE-ONLINE","MANGO CIPHER","E-COMMERCE WEBSITE","2048"]
-    var projectContent = ["TicTacToeOnline is an user vs computer game of Tic Tac Toe made in form of website. This uses minmax algorithm with a slight variation so that the user gets equal change of beating the computer.<br><br> Link - <a href=\"https://www.kartikverma.tech\/TicTacToeOnline\">www.kartikverma.tech/TicTacToeOnline</a>",
-    "This is a software(unlicensed) which encrypts and decrypts text files. This is coded in python using PyQt4 library and successfully converted into executable file. This uses self made encryption algorithm which makes it very difficult to crack.",
-    "Made an e-commerce website for sale of fashion related products. Options of filter and search product are also implemented along with basic features like view product, add to cart, add to wishlist. Products can be added, modified, removed with admin pannel.<br><br><a href=\"https://github.com/vkartik2k/e-commerce-website\">GitHub Link</a>",
-    "Made 2048 Game with help of HTML5, CSS and Vanilla JavaScript. Player can play it using arrow keys or using touch on mobile-devices. High score of player is also displayed at the top along with the current score. <br><br> Link - <a href=\"https://www.kartikverma.tech\/2048\">www.kartikverma.tech/2048</a>"]
-    var projectImage = ["./img/project1.png","./img/project2.png","./img/project3.png","./img/project4.png"]
-    $("#scHeader").html(projectHeading[id])
-    $("#scContent").html(projectContent[id])
-    $(".realImage").attr("src",projectImage[id]);
-    var underline = $("#borderHeavy")
-    underline.animate({
-      width: "80%"
-    }, "slow");
-    underline.animate({
-      width: "80px"
-    }, "slow");
-  })
-
-  // **********************************************************************************************************
-  //                                            ANIMATION FOR MAINPOST
-
-  var dataText = [ "Web Developer!", "Sport Programmer!", "Algorithm Lover!", "Sophomore at DTU!"];
-  function typeWriter(text, i, fnCallback) {
-    if (i < (text.length)) {
-      document.getElementById("mainPost").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
-      setTimeout(function() {
-        typeWriter(text, i + 1, fnCallback)
-      }, 100);
-    }
-    else if (typeof fnCallback == 'function') {
-      setTimeout(fnCallback, 700);
-    }
-  }
-    function StartTextAnimation(i) {
-      if (typeof dataText[i] == 'undefined'){
-        setTimeout(function() {
-          StartTextAnimation(0);
-        }, 700);
-      }
-    if (i < dataText[i].length) {
-      typeWriter(dataText[i], 0, function(){
-        StartTextAnimation(i + 1);
-      });
-    }
-  }
-  setTimeout(StartTextAnimation(0),100)
-  // **********************************************************************************************************
-})
+function onScroll(event) {
+	var scrollPos = $(document).scrollTop();
+	$('.header a').each(function () {
+		var currLink = $(this);
+		var refElement = $(currLink.attr("href"));
+		if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+			$('.header a div').removeClass("selected");
+			currLink.addClass("selected");
+		}
+		else {
+			currLink.removeClass("selected");
+		}
+	});
+}
